0112c27cd1010a12442c90dae56bf9dceef73af8
isMbumbu <andrewambuka9@gmail.com>
2025-04-09 15:53:32 +0300

    few changes

--- myapi.py
+++ myapi.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI , Path
+from typing import Union, Optional
+from pydantic import BaseModel
 app=FastAPI()
 
 students={
@@ -10,10 +12,15 @@ students={
     2: {
         "name":"Drew",
         "age":13,
-        "class":"Form 2 Blue"
+        "year":"Form 2 Blue"
     }
 
 }
+
+class Student(BaseModel):
+    name: str
+    age: int
+    year: str
 
 @app.get("/")
 def home():
@@ -24,5 +31,21 @@ def all_students():
     return students
 
 @app.get("/get_students/{student_id}")
-def get_students(student_id: int=Path(None, description="The ID of the student")):
+def get_students(student_id: int=Path(..., description="The ID of the student", gt=0)):
     return students[student_id]
+
+
+@app.get("/get_student_by_name")
+def get_student_by_name(student_name: str):
+    for student_id in students:
+        if students[student_id]["name"] == student_name:
+            return students[student_id]
+    return {"Data": "not found"}  
+
+@app.post("/create_student/{student_id}")
+def create_student(student_id : int, student: Student):
+    if student_id in students:
+        return {"Error":"ID exists already"}
+    students[student_id]= Student
+    return students[student_id]
+     
