81cb96c6de771f3d7e57bcdd12de20193bcd9531
isMbumbu <andrewambuka9@gmail.com>
2025-04-09 12:39:10 +0300

    update myapi

--- myapi.py
+++ myapi.py
@@ -1,7 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI , Path
 app=FastAPI()
+
+students={
+    1: {
+        "name":"John",
+        "age":17,
+        "class":"Form 1 Red"
+    },
+    2: {
+        "name":"Drew",
+        "age":13,
+        "class":"Form 2 Blue"
+    }
+
+}
 
 @app.get("/")
 def home():
     return {"name":"first Data"}
 
+@app.get("/all_students")
+def all_students():
+    return students
+
+@app.get("/get_students/{student_id}")
+def get_students(student_id: int=Path(None, description="The ID of the student")):
+    return students[student_id]
